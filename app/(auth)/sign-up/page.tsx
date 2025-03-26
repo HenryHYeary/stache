20da27df857290c978027b84f4537db00500e347
Henry Yeary <henmo@LAPTOP-A37FGP66>
2025-03-25 20:38:03 -0500

    Finish page layout and add assets to public directory.

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const SignIn = () => {
+const SignUp = () => {
   return <div>Sign Up</div>;
 };
 
-export default SignIn;
+export default SignUp;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex h-screen justify-center items-center bg-orange-200">
+    <div className="flex h-screen just
+    ify-center items-center bg-orange-200">
       <h1 className="text-blue-500 font-semibold">
         Stache - A Homemade Storage Solution
       </h1>

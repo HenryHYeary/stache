846255be34e2de52436d343ed185cc284640cd25
Henry Yeary <henry-yeary@Henrys-Mac-mini.local>
2025-03-29 17:36:10 -0500

    Flesh out more of sidebar component and make stylistic changes to placeholder home page.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex h-screen justify-center items-center bg-[#EA6365]">
-      <h1 className="text-[#ADD8E6] font-semibold">
+    <div className="flex h-screen justify-center items-center bg-[#F2F4F8] rounded-lg">
+      <h1 className="text-[34px] leading-[42px] font-bold">
         Stache - A Homemade Storage Solution
       </h1>
     </div>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <aside className="remove-scrollbar hidden h-screen w-[90px] flex-col overflow-auto px-5 py-7 sm:flex lg:w-[280px] xl:w-[325px]">
+      <Link href="/">
+        <Image 
+          src="/assets/icons/logo-full-brand.svg"
+          alt="logo" 
+          width={160}
+          height={50}
+          className="hidden h-auto lg:block"
+        />
+      </Link>
+    </aside>
   )
 }
 

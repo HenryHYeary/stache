85201bf0744af3f23cd4c4595020100fb9442aa3
Henry Yeary <henry-yeary@Henrys-Mac-mini.local>
2025-03-29 14:57:23 -0500

    Add component files for home page layout elements.

---
+++ components/Header.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Button } from "./ui/button";
+import Image from "next/image";
+import Search from "./Search";
+import FileUploader from "./FileUploader";
+
+const Header = () => {
+  return (
+    <header className="hidden items-center justify-between gap-5 p-5 sm:flex lg:py-7 xl:gap-10">
+      <Search />
+
+      <div className="flex-center min-w-fit gap-4">
+        <FileUploader />
+
+        <form>
+          <Button type="submit" className="flex-center h-[52px] min-w-[54px] items-center rounded-full bg-[#FA7275]/10 p-0 text-[#FA7275] shadow-none transition-all hover:bg-[#FA7275]/20">
+            <Image 
+              src="/assets/icons/logout.svg"
+              alt="logo"
+              width={24}
+              height={24}
+              className="w-6"
+            />
+          </Button>
+        </form>
+      </div>
+    </header>
+  )
+}
+
+export default Header;

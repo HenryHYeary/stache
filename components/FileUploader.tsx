cf81720b8bc29fe2642e410cb34f5a94195b9046
Henry Yeary <henmo@LAPTOP-A37FGP66>
2025-04-07 17:47:00 -0500

    Added minor UI changes to File Uploader component.

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,8 +1,36 @@
-import React from "react";
+"use client";
 
-const FileUploader = () => {
+import React, {useCallback} from 'react'
+import {useDropzone} from 'react-dropzone'
+import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
+import Image from 'next/image';
+
+interface FileUploaderProps {
+  ownerId: string,
+  accountId: string,
+  className?: string
+}
+
+const FileUploader = ({ ownerId, accountId, className }: FileUploaderProps) => {
+  const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
   return (
-    <div>FileUploader</div>
+    <div {...getRootProps()} className="cursor-pointer">
+    <input {...getInputProps()} />
+    <Button type="button" className={cn("uploader-button", className)}>
+      <Image src="/assets/icons/upload.svg" alt="upload" width={24} height={24} />
+    </Button>
+    {
+      isDragActive ?
+        <p>Drop the files here ...</p> :
+        <p>Drag 'n' drop some files here, or click to select files</p>
+    }
+  </div>
   )
 }
 
